d1d4cff78aea4a23cca915d9bcab7d32e115ecfd
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-08-25 16:13:55 +0200

    initial commit

---
+++ gensamps.py
@@ -0,0 +1,30 @@
+import numpy as np
+from scipy.stats import norm
+from pathlib import Path
+
+np.random.seed(1)
+
+n_draws = 1000
+
+filename = Path('newtonsConstantWithLabels.dat')
+exp_folder = Path('experiments').resolve()
+if not exp_folder.exists():
+    exp_folder.mkdir()
+
+data  = np.genfromtxt(filename, names = True, dtype = (float, float, 'U10'))
+
+means = data['value']
+std   = data['sigma']
+names = data['label']
+
+mins = []
+maxs = []
+
+for m, s, n in zip(means, std, names):
+    samps = norm(loc = m, scale = s).rvs(n_draws)
+    mins.append(samps.min())
+    maxs.append(samps.max())
+    np.savetxt(Path(exp_folder, n+'.txt'), samps)
+
+print('Min G: {} Max G: {}'.format(np.min(mins), np.max(maxs)))
+

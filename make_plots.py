abeba4db5b048678504404b83c82f3072d70b976
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-08-25 23:35:32 +0200

    working

--- make_plots.py
+++ make_plots.py
@@ -3,28 +3,104 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 # This import is just for plotting purposes
 from figaro.utils import plot_median_cr
+from figaro.cumulative import fast_cumulative
 from par_inference import out_folder
+from seaborn import color_palette
 
+palette =  color_palette('Paired', 3)
+
+codata_val = 6.67430
+codata_err = 0.00015
 
 models = ['UN', 'JF', 'IG']
 
-colors = {'UN':'r',
-          'JF':'b',
-          'IG':'g',
+colors = {'UN':palette[1],
+          'JF':palette[0],
+          'IG':palette[2],
          }
 
-data = np.genfromtxt('newtonsConstantWithLabels.dat')
+linestyle = {'UN': '-',
+             'JF': '--',
+             'IG': '-.',
+            }
+
+data = np.genfromtxt('newtonsConstantWithLabels.dat', names = True, dtype = (np.float64, np.float64, '|U10'))
+
+data['label'][np.where(data['label'] == 'HUSTT-18')] = 'HUST_T-18'
+data['label'][np.where(data['label'] == 'HUSTA-18')] = 'HUST_A-18'
+
 G = np.genfromtxt(Path(out_folder, 'prob_G_UN.txt'), names = True)['G']
 
 # Parametric inference
-medians = {mod: np.genfromtxt(Path(out_folder, 'prob_G_{0}.txt'.format(med)), names = True)['50'] for mod in models}
-G_vals_data = np.genfromtxt(Path(out_folder, 'G_vals.txt'), names = True)
-G_vals = {mod:[m, s] for mod, m, s in zip(G_vals_data['model'], G_vals_data['m'], G_vals_data['s'])}
+medians = {mod: np.genfromtxt(Path(out_folder, 'prob_G_{0}.txt'.format(mod)), names = True)['50'] for mod in models}
 
-fig, ax = plt.figure()
+fig, ax = plt.subplots()
 for mod in models:
-    ax.plot(G, medians[mod], color = colors[mod], label = mod)
-ax.legend(loc = 0, frameon = False)
-ax.set_xlabel('$G\\times10^{-11}\ [\mathrm{m}^3\mathrm{kg}^{-1}\mathrm{s}^{-2}]$')
+    medians[mod] = medians[mod]/np.sum(medians[mod]*(G[1]-G[0]))
+    ax.plot(G, medians[mod], color = colors[mod], ls = linestyle[mod], label = '$\mathrm{'+mod+'}$')
+    
+ymin, ymax = ax.get_ylim()
+hs = np.linspace(0, ymax, len(data['value'])+7)
+for hi, v, s in zip(hs[7:], data['value'][::-1], data['sigma'][::-1]):
+    ax.axhline(hi, lw = 0.03, c = 'k', ls = ':')
+    ax.errorbar(v, hi, xerr = s, color = 'orange', marker = 'o', ms = 4)
 
+# CODATA value
+ax.axhline(hs[6], lw = 0.03, c = 'k', ls = ':')
+ax.errorbar(codata_val, hs[6], xerr = codata_err, color = 'blue', marker = 'o', ms = 4)
+
+# This work
+for i, mod in enumerate(models):
+    cdf = fast_cumulative(np.ascontiguousarray(medians[mod]*(G[1]-G[0])))
+    pcs = [G[np.where(cdf < perc)].max() for perc in [0.05, 0.16, 0.50, 0.84, 0.95]]
+    ax.axhline(hs[2*i], lw = 0.03, c = 'k', ls = ':')
+    ax.errorbar(pcs[2], hs[2*i], xerr = np.atleast_2d([pcs[2]-pcs[0], pcs[4]-pcs[2]]).T, color = colors[mod], marker = 'o', ms = 4)
+    ax.axhline(hs[2*i+1], lw = 0.03, c = 'k', ls = ':')
+    ax.errorbar(pcs[2], hs[2*i+1], xerr = np.atleast_2d([pcs[2]-pcs[1], pcs[3]-pcs[2]]).T, color = colors[mod], marker = 'o', ms = 4)
+    print('{0}: {1} - {2} + {3}. Conservative: - {4} + {5}'.format(mod, pcs[2], pcs[2]-pcs[1], pcs[3]-pcs[2], pcs[2]-pcs[0], pcs[4]-pcs[2]))
+
+plt.yticks(hs[::-1], ['$\mathrm{'+l+'}$' for l in data['label']] + ['$\mathrm{CODATA}$', '$\mathrm{IG}$', '$\mathrm{IG - Conservative}$','$\mathrm{JF}$', '$\mathrm{JF - Conservative}$','$\mathrm{UN}$', '$\mathrm{UN - Conservative}$'])
+ax.grid(visible = False)
+ax.set_xlim(G.min(),G.max())
+ax.legend(loc = 0, frameon = False)
+ax.set_xlabel('$G\ [\\times10^{-11}\ \mathrm{m}^3\mathrm{kg}^{-1}\mathrm{s}^{-2}]$')
+
+ax.tick_params(axis='y', which='major', labelsize=6)
 fig.savefig(Path(out_folder, 'par_models.pdf'), bbox_inches = 'tight')
+
+# Nonparametric inference
+rec = np.genfromtxt(Path(out_folder, 'plots','prob_newtonG.txt'), names = True)
+
+fig, ax = plt.subplots()
+ax.fill_between(rec['x'], rec['95'], rec['5'], color = 'mediumturquoise', alpha = 0.5)
+ax.fill_between(rec['x'], rec['84'], rec['16'], color = 'darkturquoise', alpha = 0.5)
+# Median
+ax.plot(rec['x'], rec['50'], lw = 1, color = 'steelblue')
+
+ymin, ymax = ax.get_ylim()
+
+hs = np.linspace(0, ymax, len(data['value'])+3)
+for hi, v, s in zip(hs[3:], data['value'][::-1], data['sigma'][::-1]):
+    ax.axhline(hi, lw = 0.03, c = 'k', ls = ':')
+    ax.errorbar(v, hi, xerr = s, color = 'orange', marker = 'o', ms = 4)
+
+# CODATA value
+ax.axhline(hs[2], lw = 0.03, c = 'k', ls = ':')
+ax.errorbar(codata_val, hs[2], xerr = codata_err, color = 'blue', marker = 'o', ms = 4)
+
+# This work
+cdf = fast_cumulative(np.ascontiguousarray(rec['50']*(G[1]-G[0])))
+pcs = [rec['x'][np.where(cdf < perc)].max() for perc in [0.05, 0.16, 0.50, 0.84, 0.95]]
+ax.axhline(hs[0], lw = 0.03, c = 'k', ls = ':')
+ax.errorbar(pcs[2], hs[0], xerr = np.atleast_2d([pcs[2]-pcs[0], pcs[4]-pcs[2]]).T, color = 'steelblue', marker = 'o', ms = 4)
+ax.axhline(hs[1], lw = 0.03, c = 'k', ls = ':')
+ax.errorbar(pcs[2], hs[1], xerr = np.atleast_2d([pcs[2]-pcs[1], pcs[3]-pcs[2]]).T, color = 'steelblue', marker = 'o', ms = 4)
+    
+print('(H)DPGMM: {1} - {2} + {3}. Conservative: - {4} + {5}'.format(mod, pcs[2], pcs[2]-pcs[1], pcs[3]-pcs[2], pcs[2]-pcs[0], pcs[4]-pcs[2]))
+plt.yticks(hs[::-1], ['$\mathrm{'+l+'}$' for l in data['label']] + ['$\mathrm{CODATA}$', '$\mathrm{(H)DPGMM}$', '$\mathrm{(H)DPGMM - Conservative}$'])
+ax.grid(visible = False)
+ax.set_xlim(G.min(),G.max())
+ax.set_xlabel('$G\ [\\times10^{-11}\ \mathrm{m}^3\mathrm{kg}^{-1}\mathrm{s}^{-2}]$')
+
+ax.tick_params(axis='y', which='major', labelsize=6)
+fig.savefig(Path(out_folder, 'hdpgmm.pdf'), bbox_inches = 'tight')
--- par_inference.py
+++ par_inference.py
@@ -1,32 +1,36 @@
 import numpy as np
 from utils import G_inference, plot_distribution
 from pathlib import Path
-import cpnest
+import os
 
-models = ['UN', 'JF', 'IG']
-
-data = np.genfromtxt('newtonsConstantWithLabels.dat', names = True)
 out_folder = Path('output')
 if not out_folder.exists():
     out_folder.mkdir()
 
-means = []
-sigma = []
+models = ['UN', 'JF', 'IG']
+n_live_points = 1000
 
-for mod in models:
-    
-    M = G_inference(data['value'], data['sigma'], mod)
-    work = cpnest.CPNest(M,
-                        verbose = 0,
-                        nlive = 1000,
-                        maxmcmc = 5000,
-                        nensemble = 1,
-                        )
-    work.run()
-    post = work.posterior_samples.ravel()
-    plot_distribution(post, mod, out_folder)
-    
-    means.append(post['m'].mean())
-    sigma.append(post['s'].mean())
-    
-np.savetxt(Path(out_folder, 'G_vals.txt'), np.array([models, means, sigma]).T, header = 'model m s', fmt = '%10s , %10.3f, %10.3f')
+if __name__ == '__main__':
+    import cpnest
+    data = np.genfromtxt('newtonsConstantWithLabels.dat', names = True)
+
+    means = []
+    sigma = []
+
+    for mod in models:
+        
+        M = G_inference(data['value'], data['sigma'], mod)
+        work = cpnest.CPNest(M,
+                            verbose = 0,
+                            nlive = n_live_points,
+                            maxmcmc = 5000,
+                            nensemble = 1,
+                            )
+        work.run()
+        post = work.posterior_samples.ravel()
+        plot_distribution(post, mod, out_folder)
+        
+        means.append(post['m'].mean())
+        sigma.append(post['s'].mean())
+    # Remove files from CPNest
+    [os.remove(f) for f in ['chain_{0}_0.txt'.format(n_live_points), 'chain_{0}_0.txt_evidence_0.txt'.format(n_live_points),'cpnest.log','header.txt']]

97efd1a2608ca68220555ba8aeda6f830effc7a5
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-08-26 09:34:31 +0200

    initial commit

---
+++ run_inference.sh
@@ -0,0 +1,6 @@
+python gensamps.py
+
+python par_inference.py
+figaro-hierarchical -i experiments -o output --symbol 'G' --unit '\times 10^{-11}\ m^3km^{-1}s^{-2}' -b '[6.668, 6.678]' -s
+
+python make_plots.py

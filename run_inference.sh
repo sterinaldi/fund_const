cdc98a7a46fdeebb6fe5dd7a0893808539326578
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-09-14 11:24:59 +0100

    fixed bug

--- run_inference.sh
+++ run_inference.sh
@@ -16,9 +16,9 @@ python gensamps.py -i $file -o $outfolder
 python par_inference.py -i $file -o $outfolder -b $bounds -l $label -u $unit 2>/dev/null
 
 if [ ! -f $outfolder"/draws/posteriors_single_event.pkl" ]; then
-    figaro-hierarchical -i experiments -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -s
+    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -s
 else
-    figaro-hierarchical -i experiments -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -e
+    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -e
 fi
 
 python make_plots.py -i $file -o $outfolder -l $label -u $unit -v $codata_val -e $codata_err -n $outfolder

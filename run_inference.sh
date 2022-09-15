da95080ed4828e4ee076a459068d8d58431d3ba5
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-09-15 09:49:18 +0100

    modified structure

--- run_inference.sh
+++ run_inference.sh
@@ -3,9 +3,9 @@ source $1
 if [ ! -d $outfolder ]; then
     mkdir $outfolder
 fi
-python least_squares.py -i $file
-python sigma_level.py -i $file -o $outfolder
-python gensamps.py -i $file -o $outfolder
+python fund_const/least_squares.py -i $file
+python fund_const/sigma_level.py -i $file -o $outfolder
+python fund_const/gensamps.py -i $file -o $outfolder
 
 python par_inference.py -i $file -o $outfolder -b $bounds -l $label -u $unit 2>/dev/null
 
@@ -15,4 +15,4 @@ else
     figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -e
 fi
 
-python make_plots.py -i $file -o $outfolder -l $label -u $unit -v $codata_val -e $codata_err -n $outfolder
+python fund_const/make_plots.py -i $file -o $outfolder -l $label -u $unit -v $codata_val -e $codata_err -n $outfolder

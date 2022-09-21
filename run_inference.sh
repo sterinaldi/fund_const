863155b8479ba6a980a2dca70281e7edbee0efd1
Stefano Rinaldi <stefano.rinaldi@ligo.org>
2022-09-21 09:49:53 +0200

    added number of draws

--- run_inference.sh
+++ run_inference.sh
@@ -10,9 +10,9 @@ python fund_const/gensamps.py -i $file -o $outfolder
 python fund_const/par_inference.py -i $file -o $outfolder -b $bounds -l $label -u $unit 2>/dev/null
 
 if [ ! -f $outfolder"/draws/posteriors_single_event.pkl" ]; then
-    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -s
+    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -s --draws 1000 --se_draws 100
 else
-    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -e
+    figaro-hierarchical -i $outfolder"/experiments" -o $outfolder --symbol $label --unit $unit -b $bounds --name $outfolder -e --draws 1000
 fi
 
 python fund_const/make_plots.py -i $file -o $outfolder -l $label -u $unit -v $codata_val -e $codata_err -n $outfolder
